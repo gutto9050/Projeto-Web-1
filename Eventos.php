547f1d88f4db48b504838d3cbd3310bd99728ab6
Gustavo Santos <monteiro.eleven@gmail.com>
2023-05-16 20:00:39 -0300

    Eventos 1.0

---
+++ Eventos.php
@@ -0,0 +1,36 @@
+<?php 
+
+$evento = [
+
+    [
+        'id' => 1,
+        'nome' => 'Guardiões da Galaxai Vol.3 Pré-estreia' ,
+        'local' => 'Vitoria da Conquista - BA , Shopping conquista Sul',
+        'data' => "29/05/2023",
+        'preco' => 'R$ 150',
+        'hora' => '20:00',
+        'tipo' => 'Cinema',
+        'nivel' => 'Deluxe',
+        'descricao' => 'Sinopse.... Evento de pré-estreia all inclusive com brindes. ',
+        'imagem' => '/Imagen/poster-guardioes-da-galaxia-vol-3-.jpg'
+
+    ],
+
+    [
+        'id' => 2,
+        'nome' => 'Homem-Aranha: Através do Aranhaverso Pré-estreia' ,
+        'local' => 'São Paulo - SP , Shopping Boulevard',
+        'data' => "02/06/2023",
+        'preco' => 'R$ 250',
+        'hora' =>  '22:30' ,
+        'tipo' => 'Cinema',
+        'nivel' => 'Ultimate',
+        'descricao' => 'Sinopse.... Evento de pré-estreia all inclusive com brindes e jogos e intrevistas. ',
+        'imagem' => '/Imagen/Spider-Man.jpg'
+    ]
+
+
+]
+
+
+?>
---
+++ index.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ingresso</title>
+</head>
+<body>
+        
+    <?php 
+        include "Eventos.php";
+                
+        foreach ( $evento as $eventos ) { ?>
+
+                <h1> <?=$eventos["nome"]?> </h1>
+
+                <img src="<?=$eventos["imagem"]?>"  alt="Poster Evento">
+
+              
+                <p> <?=$eventos['local']?> </p>
+                <p> <?=$eventos['data']?> </p>
+                <p> <?=$eventos['hora']?> </p>
+                <p> <?=$eventos['preco']?> </p>
+                <p> <?=$eventos['tipo']?> </p>
+                <p> <?=$eventos['nivel']?> </p>
+
+                <p> <?=$eventos['descricao']?> </p>
+
+
+
+
+
+
+    <?php } ?>
+
+</body>
+</html>
