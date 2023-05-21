a5852ab3bd4741386733442ca77b0bf98ecca098
Erik Louhan <91701590+eriklouhan@users.noreply.github.com>
2023-05-21 20:39:28 -0300

    Update Eventos.php

--- Eventos.php
+++ Eventos.php
@@ -12,7 +12,7 @@ $evento = [
         "tipo" => "Cinema",
         "nivel" => "Deluxe",
         "descricao" => "Sinopse.... Evento de pré-estreia all inclusive com brindes. ",
-        "imagem" => "imagen/poster-guardioes-da-galaxia-vol-3-.jpg"
+        "imagem" => "imagem/poster-guardioes-da-galaxia-vol-3-.jpg"
        
 
     ],
@@ -27,7 +27,7 @@ $evento = [
         "tipo" => "Cinema",
         "nivel" => "Ultimate",
         "descricao" => "Sinopse.... Evento de pré-estreia all inclusive com brindes e jogos e intrevistas. ",
-        "imagem" => "Imagen/Spider-Man.jpg"
+        "imagem" => "Imagem/Spider-Man.jpg"
        
     ],
 
@@ -41,11 +41,11 @@ $evento = [
     "tipo" => "cinema",
     "nivel" => "live action",
     "descricao"=> "Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
-    "imagem" => "imagen/barbie.jpg"
+    "imagem" => "imagem/barbie.jpg"
     ],
 
     [
-        "id" => 4,
+       "id" => 4,
         "nome" => "Pequena Sereia",
         "local" => "Vitoria da Conquista - BA , Shopping conquista Sul",
         "data" => "26/05/2023",
@@ -54,11 +54,11 @@ $evento = [
         "tipo" => "Cinema",
         "nivel" => "live action",
         "descricao" => "Uma jovem sereia faz um acordo com uma bruxa do mar para trocar sua bela voz por pernas humanas para que possa descobrir o mundo acima da água e impressionar um príncipe.",
-        "imagem" => "imagen/apequenasereia.jpg"
+        "imagem" => "imagem/apequenasereia.jpg"
     ],
 
     [
-        "id" => 5,
+       "id" => 5,
         "nome" => "O Exorcista do Papa",
         "local" => "Vitoria da Conquista - Ba , Ariat Films",
         "data" => "01/06/2023",
@@ -67,11 +67,11 @@ $evento = [
         "tipo" => "Cinema",
         "nivel" => "continuação",
         "descricao" => "O padre Gabriele Amorth, exorcista do Vaticano, luta contra Satanás e demônios possuidores de inocentes. Um retrato detalhado de um padre que realizou mais de 100 mil exorcismos em sua vida." ,
-        "imagem" => "imagen/exorcista.jpg"
+        "imagem" => "imagem/exorcista.jpg"
     ],
 
     [
-        "id" => 6,
+       "id" => 6,
         "nome" => "Peter Pan e Wendy",
         "local" => "Vitoria da Conquista - Ba, Ariat Films",
         "data" => "22/05/2023",
@@ -80,50 +80,65 @@ $evento = [
         "tipo" => "Cinema",
         "nivel" => "live action",
         "descricao" => "Wendy Darling, uma jovem com medo de deixar sua casa de infância para trás, conhece Peter Pan, um menino que se recusa a crescer. Com seus irmãos e uma fada, Tinker Bell, ela viaja com Peter para o mundo mágico da Terra do Nunca. Lá, ela encontra um pirata perverso chamado Capitão Gancho, e embarca em uma aventura emocionante que vai mudar sua vida para sempre.",
-        "imagem" => "imagen/peterpan.jpg"
+        "imagem" => "imagem/peterpan.jpg"
     ]
-
     ];
-
- 
- $shows[
-
+$shows = [
     [
-    "id" => 7,
-    "nome" => "Renaissance Tour",
-    "local" => "Belo Horizonte - MG, Arena BH Beach",
-    "preco" => "R$ 900",
-    "hora" => "19:00",
-    "tipo" => "show",
-    "nivel" => "turnê",
-    "imagem" => "imagen/beyonce.jpg"
+        "id" => 7,
+        "nome" => "Renaissance Tour",
+        "local" => "Belo Horizonte - MG, Arena BH Beach",
+        "data" => "18/08/2023",
+        "preco" => "R$ 900",
+        "hora" => "19:00",
+        "tipo" => "show",
+        "nivel" => "turnê",
+        "imagem" => "imagem/renaissance.jpg"
     ],
 
     [
         "id" => 8,
         "nome" => "The eras Tour",
+        "data" => "20/08/2023",
         "local" => "Rio de Janeiro - RJ, Arena Tuiuti",
         "preco" => "R$ 2500",
          "hora" => "19:30",
         "tipo" => "show",
         "nivel" => "turnê",
-        "imagem" => "imagen/theras.jpg"
+        "imagem" => "imagem/theeras.jpg"
     ],
 
     [
         "id" => 9,
         "nome" => "The Celebration Tour",
+        "data" => "29/06/2023",
         "local" => "Rio de Janeiro, Parque dos atletas",
         "preco" => "R$ 3500",
         "hora" => "20:00",
         "tipo" => "show",
         "nivel" => "turnê",
-        "imagem" => "imagen/celebration.jpg"
-    ]
+        "imagem" => "imagem/celebration.jpg"
+    ],
 
+    [
+        "id" => 10,
+        "nome" => "Ensaios da Anitta",
+        "data" => "28/05/2023",
+        "local" => "Centro de Convenções",
+        "preco" => "R$ 110",
+        "hora" => "21:00",
+        "tipo" => "show",
+        "nivel" => "festival",
+        "imagem" => "imagem/ensaiosdaanitta.jpg"
+
+
+        
+    ]
     
- ];
+    ];
 
 
 ?>
+
+
 
