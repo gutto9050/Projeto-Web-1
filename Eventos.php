3dbab5e6430fb01a5d23dfbc9eb1c49147fd6aaa
renatim99 <85313770+renatim99@users.noreply.github.com>
2023-05-26 14:26:16 -0300

    Update Eventos.php

--- Eventos.php
+++ Eventos.php
@@ -4,13 +4,14 @@ $evento = [
 
     [
         "id" => 1,
-        "nome" => "Guardiões da Galaxai Vol.3 Pré-estreia" ,
+        "nome" => "Guardiões da Galaxia Vol.3 Pré-estreia" ,
         "local" => "Vitoria da Conquista - BA , Shopping conquista Sul",
         "data" => "29/05/2023",
         "preco" => "R$ 150",
         "hora" => "20:00",
         "tipo" => "Cinema",
         "nivel" => "pre-estreia",
+        "genero" => "Ação",
         "descricao" => "Em Guardiões da Galáxia Vol. 3, o amado grupo de desajustados busca se estabelecer em Lugar Nenhum, mas não demora muito para que suas vidas sejam reviradas pelos ecos do passado turbulento de Rocket (Bradley Cooper). Ainda se recuperando da perda de Gamora (Zoe Saldana), após os acontecimentos de Vingadores: Guerra Infinita (2018), Peter Quill (Chris Pratt) reúne sua equipe para defender o universo e um companheiro de equipe. Mas esta missão pode significar o fim dos Guardiões como conhecemos, se ela não for bem-sucedida. Encerramento da trilogia iniciada em 2014, sendo a continuação de Guardiões da Galáxia Vol. 2 (2017).+
         Evento de pré-estreia all inclusive com brindes. ",
         "imagem" => "Imagen/poster-guardioes-da-galaxia-vol-3-.jpg"
@@ -27,6 +28,7 @@ $evento = [
         "hora" =>  "22:30" ,
         "tipo" => "Cinema",
         "nivel" => "Pre-estreia",
+        "genero" => "Ação",
         "descricao" => "Homem-Aranha: Através do Aranhaverso, é a continuação do vencedor do Oscar Homem-Aranha: No Aranhaverso, de 2018, que acompanha Miles Morales (Shameik Moore), o simpático Homem-Aranha do Brooklyn. Neste novo capítulo, Miles está de volta para uma nova missão em sua agitada vida como super herói. No novo filme, Morales é transportado para uma aventura épica através do multiverso, e deve unir forças com a mulher-aranha Gwen Stacy (Hailee Steinfeld) e um novo time de Pessoas-Aranha, formado por heróis de diversas dimensões. No entanto, tudo muda quando os heróis entram em conflito sobre como lidar com uma nova ameaça, e Miles se vê em um impasse. E para piorar ainda mais a situação, eles precisam enfrentar um vilão muito mais poderoso do que qualquer coisa que já tenham encontrado antes. Agora, para salvar as pessoas que ele mais ama no mundo, Miles deve redefinir o que significa ser um super herói.
          Evento de pré-estreia all inclusive com brindes e jogos e intrevistas. ",
         "imagem" => "Imagen/Spider-Man.jpg"
@@ -42,6 +44,7 @@ $evento = [
     "hora" => "18:30",
     "tipo" => "cinema",
     "nivel" => "Estreia",
+    "genero" => "infantil",
     "descricao"=> "No mundo mágico das Barbies, Barbieland, todas as versões de Barbie vivem em harmonia e sem preocupações. Todavia, uma das bonecas (Margot Robbie) começa a perceber que não se encaixa como as outras. Após ser expulsa, Barbie e seu fiel Ken (Ryan Gosling) - que decide acompanhar a amada - são colocados no mundo real e forçados a lutar com as dificuldades de não serem mais apenas bonecos. Nesta aventura no mundo real, Barbie descobre que a beleza está no interior de cada um.",
     "imagem" => "Imagen/barbie.jpg"
     ],
@@ -54,6 +57,7 @@ $evento = [
         "preco" => "R$ 150",
         "hora" => "14:30",
         "tipo" => "Cinema",
+        "genero" => "infantil",
         "nivel" => "Estreia",
         "descricao" => "Remake live action do clássico desenho animado A Pequena Sereia, de 1989, da Disney. A caçula das filhas do Rei Tritão (Javier Bardem), Ariel (Halle Bailey) é uma bela e espirituosa jovem sereia com sede de aventura. Desejando descobrir mais sobre o mundo além do mar, Ariel visita a superfície e se apaixona intensamente pelo arrojado Príncipe Eric (Jonah Hauer-King), ao salvá-lo de um naufrágio. Mas para procurá-lo em terra firme e se aproximar do príncipe humano, a sereia pede ajuda à bruxa do mar, Úrsula (Melissa McCarthy), e aceita ceder sua voz para que a feiticeira lhe dê pernas. Agora, ela terá o desafio de se comunicar com o rapaz ao experimentar a vida em terra firme, além de entrar em conflito com os valores de sua família. A Pequena Sereia é um conto de fadas literário escrito pelo autor dinamarquês Hans Christian Andersen, publicado pela primeira vez em 1837.",
         "imagem" => "Image/apequenasereia.jpg"
@@ -67,6 +71,7 @@ $evento = [
         "preco" => "R$ 100",
         "hora" => "20:30",
         "tipo" => "Cinema",
+        "genero" => "Terror",
         "nivel" => "Estreia",
         "descricao" => "Inspirado nos arquivos reais do Padre Gabriele Amorth, Chefe Exorcista do Vaticano. O padre realizou mais de 100.000 exorcismos em sua vida e faleceu em 2016 aos 91 anos. Amorth escreveu duas memórias – An Exorcist Tells His Story e An Exorcist: More Stories – e detalhou suas experiências lutando contra Satanás e demônios que agarraram e possuíram as pessoas com seu mal. O filme, sendo o retrato do personagem da vida real, acompanha Amorth (Russell Crowe) enquanto ele investiga a terrível possessão de um menino e acaba descobrindo uma conspiração secular que o Vaticano tentou desesperadamente proteger e manter no esquecimento.
         " ,
@@ -81,6 +86,7 @@ $evento = [
         "preco" => "R$ 100",
         "hora" => "15:30",
         "tipo" => "Cinema",
+        "genero" => "infantil/Ação",
         "nivel" => "Estreia",
         "descricao" => "Wendy Darling, uma jovem com medo de deixar sua casa de infância para trás, conhece Peter Pan, um menino que se recusa a crescer. Com seus irmãos e uma fada, Tinker Bell, ela viaja com Peter para o mundo mágico da Terra do Nunca. Lá, ela encontra um pirata perverso chamado Capitão Gancho, e embarca em uma aventura emocionante que vai mudar sua vida para sempre.",
         "imagem" => "Imagen/peterpan.jpg"
@@ -93,6 +99,7 @@ $evento = [
          "preco" => "R$ 700",
          "hora" => "17:30",
          "tipo" => "Cinema",
+         "genero" => "Terror",
          "nivel" => "Rexibição",
          "descricao" => "Em It - Capítulo 2, 27 anos depois dos eventos de It - A Coisa, Mike (Isaiah Mustafa) percebe que o palhaço Pennywise (Bill Skarsgard) está de volta à cidade de Derry. Ele convoca os antigos amigos do Clube dos Otários para honrar a promessa de infância e acabar com o inimigo de uma vez por todas. Mas quando Bill (James McAvoy), Beverly (Jessica Chastain), Ritchie (Bill Hader), Ben (Jay Ryan) e Eddie (James Ransone) retornam às suas origens, eles precisam se confrontar a traumas nunca resolvidos de suas infâncias, e que repercutem até hoje na vida adulta.",
          "imagem" => "Imagen/it-2.jpg"
@@ -106,6 +113,7 @@ $evento = [
         "hora" => "12:45",
         "tipo" => "Cinema",
         "nivel" => "Rexibição",
+        "genero" => "Drama/Crime",
         "descricao" => "Em Coringa, Arthur Fleck (Joaquin Phoenix) trabalha como palhaço para uma agência de talentos e, toda semana, precisa comparecer a uma agente social, devido aos seus conhecidos problemas mentais. Após ser demitido, Fleck reage mal à gozação de três homens em pleno metrô e os mata. Os assassinatos iniciam um movimento popular contra a elite de Gotham City, da qual Thomas Wayne (Brett Cullen) é seu maior representante.
         ",
         "imagem" => "Imagen/coringa.jpeg"
@@ -119,6 +127,7 @@ $evento = [
         "hora" => "14:45",
         "tipo" => "Cinema",
         "nivel" => "Rexibição",
+        "genero" => "Ficção Cientifica/Aventura",
         "descricao" => "Um jovem (Michael J. Fox) aciona acidentalmente uma máquina do tempo construída por um cientista (Christopher Lloyd) em um Delorean, retornando aos anos 50. Lá conhece sua mãe (Lea Thompson), antes ainda do casamento com seu pai, que fica apaixonada por ele. Tal paixão põe em risco sua própria existência, pois alteraria todo o futuro, forçando-o a servir de cupido entre seus pais.
         ",
         "imagem" => "Imagen/futuro.jpeg.webp"
@@ -132,6 +141,7 @@ $evento = [
         "hora" => "20:00",
         "tipo" => "Cinema",
         "nivel" => "Estreia",
+        "genero" => "Aventura/Fantasia",
         "descricao" => "Mario é um filme baseado na série de video games, Super Mario Bros, da Nintendo. Em Super Mario Bros. - O Filme, Mario (Chris Pratt) é um encanador qualquer no bairro de Brooklyn junto com seu irmão Luigi (Charlie Day). Um dia, Mario e Luigi vão para no reino dos cogumelos, governado pela Princesa Peach (Anya Taylor-Joy), mas ameaçado pelo rei dos Koopas, Bowser (Jack Black), que vai fazer de tudo para conseguir reinar todos os lugares. É então quando Luigi é raptado por Bowser e o usa para procurar Mario, o único capaz de deter o Koopa e reestabelexcer a paz. Mario terá que aprender como viver nesse novo reino perigoso, passando por vários biomas, aprender a dirigir carros, utilizar itens que o fazem soltar bolas de fogo das mãos, virar um animal e andar em plataformas nada confiáveis. Também estará acompanhados de amigos, como Toad (Keegan-Michael Key) e Donkey Kong (Seth Rogan).
 
         ",
@@ -146,6 +156,7 @@ $evento = [
         "hora" => "22:00",
         "tipo" => "Cinema",
         "nivel" => "Evento especiasl",
+        "genero" => "Aventura",
         "descricao" => "Inspirado na série de livros de Frank Herbert, Duna se passa em um futuro longínquo. O Duque Leto Atreides administra o planeta desértico Arrakis, também conhecido como Duna, lugar de única fonte da substância rara chamada de melange, usada para estender a vida humana, chegar a velocidade da luz e garantir poderes sobrehumanos. Para isso ele manda seu filho, Paul Atreides (Timothée Chalamet), um jovem brilhante e talentoso que nasceu para ter um grande destino além de sua imaginação, e seus servos e concubina Lady Jessica (Rebecca Fergunson), que também é uma Bene Gesserit. Eles vão para Duna, afim de garantir o futuro de sua família e seu povo. Porém, uma traição amarga pela posse da melange faz com que Paul e Jessica fujam para os Fremen, nativos do planeta que vivem nos cantos mais longes do deserto.
         ",
         "imagem" => "Imagen/dune.jpeg"
@@ -164,6 +175,8 @@ $shows = [
         "hora" => "19:00",
         "tipo" => "show",
         "nivel" => "turnê",
+        "genero" => "Pop",
+        "descricao" => "Renaissance World Tour é a sexta turnê em curso da cantora estadunidense Beyoncé, em suporte ao seu sétimo álbum de estúdio, Renaissance.", 
         "imagem" => "Imagen/renaissance.jpg"
     ],
 
@@ -176,6 +189,8 @@ $shows = [
          "hora" => "19:30",
         "tipo" => "show",
         "nivel" => "turnê",
+        "genero" => "Pop",
+        "descricao" =>" The Eras Tour é a sexta turnê em curso da cantora e compositora estadunidense Taylor Swift, assim como a sua segunda turnê em estádios após a Reputation Stadium Tour.",
         "imagem" => "Imagen/theeras.jpg"
     ],
 
@@ -188,6 +203,8 @@ $shows = [
         "hora" => "20:00",
         "tipo" => "show",
         "nivel" => "turnê",
+        "genero" => "Pop",
+        "descricao" => "The Celebration Tour é a futura décima segunda turnê da cantora americana Madonna. Considerada a maior produção que a cantora já fez, contando com canções de todas as épocas de sua carreira,a turnê está programada para começar em 15 de julho de 2023, na Rogers Arena, em Vancouver, Canadá, e terminar em 30 de janeiro de 2024 no Palacio de los Deportes, em Cidade do México, México, visitando cidades da América do Norte e da Europa, ao longo de 84 datas.[4][5] Como primeira turnê retrospectiva de Madonna, The Celebration Tour destaca seus mais de 40 anos de carreira musical",
         "imagem" => "Imagen/celebration.jpg"
     ],
 
@@ -200,6 +217,8 @@ $shows = [
         "hora" => "21:00",
         "tipo" => "show",
         "nivel" => "festival",
+        "genero" => "Pop",
+        "descricao" => "F",
         "imagem" => "Imagen/ensaiosdaanitta.jpg"
 
 
@@ -214,6 +233,8 @@ $shows = [
         "hora" => "22:00",
         "tipo" => "show",
         "nivel" => "Gravação de CD",
+        "genero" => "Sertanejo",
+        "descricao" => "  Henrique e Juliano é uma dupla sertaneja formada pelos irmãos Ricelly Henrique Tavares Reis (23 de maio de 1989) e Edson Alves dos Reis Junior (27 de novembro de 1990) ambos nascidos na cidade de Palmeirópolis, interior do estado de Tocantins.",
         "imagem" => "Imagen/banner-destaque-desktop.jpeg" 
      ]
     
